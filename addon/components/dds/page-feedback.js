688f8fbe94793e8fb74d778a799625bc1835e8fc
Arman <arman.zrb@gmail.com>
2019-08-21 17:08:32 +1000

    Move feeback validation out of dds

--- addon/components/dds/page-feedback.js
+++ addon/components/dds/page-feedback.js
@@ -1,14 +1,8 @@
 import Component from '@ember/component';
-import { buildValidations, validator } from 'ember-cp-validations';
 import { inject } from '@ember/service';
 import layout from '../../templates/components/dds/page-feedback';
 
-const Validations = buildValidations({
-  'feedback.issue': validator('presence', true),
-  'feedback.task': validator('presence', true),
-});
-
-export default Component.extend(Validations, {
+export default Component.extend({
   layout,
   router: inject(),
   activated: false,
