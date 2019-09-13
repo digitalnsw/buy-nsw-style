a3bb8b0b501149cff1e95b038da8a4b6a19fb0c3
Arman <arman.zrb@gmail.com>
2019-09-13 13:37:05 +1000

    Improve the image cropper

--- addon/components/dds/avatar-upload.js
+++ addon/components/dds/avatar-upload.js
@@ -4,7 +4,6 @@ import layout from '../../templates/components/dds/avatar-upload';
 
 export default Component.extend({
   layout,
-  croppedAvatar: null,
 
   toBlob(canvas, then) {
     if(canvas.toBlob) {
@@ -22,41 +21,31 @@ export default Component.extend({
   },
 
   actions: {
-    upload() {
-      let component = this;
-      this.get('fileService').get('uploadAvatar').perform(
-        this.get('cropBlob'),
-        'cropped.png',
-        'image/png',
-        function(body) {
-          component.set('field', body.id);
-          component.set('hasChanged', true);
-          component.set('imgSrc', null);
-          component.set('croppedAvatar', null);
-          component.set('cropBlob', null);
-          if(component.get('signal') != undefined) {
-            component.incrementProperty('signal');
-          }
-        }
-      );
-    },
     crop() {
       let croppedImage = this.get('cropper').getCroppedCanvas();
-      this.set('croppedAvatar', croppedImage);
       let component = this;
       this.toBlob(croppedImage, function (blob) {
-        component.set('cropBlob', blob);
+        component.get('fileService').get('uploadAvatar').perform(
+          blob,
+          'cropped.png',
+          'image/png',
+          function(body) {
+            component.set('field', body.id);
+            component.set('hasChanged', true);
+            component.set('imgSrc', null);
+            if(component.get('signal') != undefined) {
+              component.incrementProperty('signal');
+            }
+          }
+        );
       });
     },
     cancel() {
       this.set('imgSrc', null);
-      this.set('croppedAvatar', null);
-      this.set('cropBlob', null);
     },
     filePicked(file) {
       let component = this;
       component.set('imgSrc', null);
-      component.set('croppedAvatar', null);
       file.readAsDataURL().then((result) => {
         component.set('imgSrc', result);
       })
