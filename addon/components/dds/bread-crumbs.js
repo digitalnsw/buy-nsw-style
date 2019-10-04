0b175d0f6d5dcf4bf86e26d45b1a0d3e0d5d241a
Adrian Hall <adohall@gmail.com>
2019-10-04 12:16:43 +1000

    SO-355 - Supplier dashboard

--- addon/components/dds/bread-crumbs.js
+++ addon/components/dds/bread-crumbs.js
@@ -2,5 +2,9 @@ import Component from '@ember/component';
 import layout from '../../templates/components/dds/bread-crumbs';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: 'nav',
+  classNames: ['au-breadcrumbs'],
+  attributeBindings: ['customLabel:aria-label'],
+  customLabel: 'breadcrumb'
 });
--- addon/components/dds/template-body.js
+++ addon/components/dds/template-body.js
@@ -3,4 +3,5 @@ import layout from '../../templates/components/dds/template-body';
 
 export default Component.extend({
   layout,
+  classNames: ['container-fluid less-space']
 });
