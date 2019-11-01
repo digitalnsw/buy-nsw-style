e5702ed02a9b3c2928319c635ab63ad986a7ab60
Adrian Hall <adohall@gmail.com>
2019-11-01 15:22:28 +1100

    SO-401 - drop-down nav in header

--- addon/components/dds/drop-down.js
+++ addon/components/dds/drop-down.js
@@ -1,7 +1,5 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/dds/drop-down';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
@@ -10,10 +8,6 @@ export default Component.extend({
   attributeBindings: ['customLabel:aria-label'],
   customLabel: 'navigation',
   isOpen: false,
-  router: service(),
-  //currentRouteName: computed.notEmpty('router.currentRoute.name'), // same result as line below
-  currentRouteName: computed.notEmpty('router.currentRouteName'),
-  currentPath: computed.notEmpty('router.currentPath'),
   
   actions: {
     onToggle(){
