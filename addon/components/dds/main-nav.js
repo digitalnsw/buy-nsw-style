4e0bc3f27afc2c074b6fc5b93dc97fccdc09727d
Adrian Hall <adohall@gmail.com>
2019-11-05 11:27:50 +1100

    SO-401 - convert drop-down and main nav to #each

--- addon/components/dds/main-nav.js
+++ addon/components/dds/main-nav.js
@@ -9,4 +9,17 @@ export default Component.extend({
   noBorder: false,
   attributeBindings: ['customLabel:aria-label'],
   customLabel: 'main navigation',
+  willRender() {
+    // Set the "mainlinks" property to a JavaScript array of link objects
+    // each link object contains name (link text), route, and (optional) param
+    // combination of https://guides.emberjs.com/release/templates/displaying-a-list-of-items/ and 
+    // https://guides.emberjs.com/release/templates/displaying-the-keys-in-an-object/
+    this.set('mainlinks', [
+      { name: 'Supplier list', route: 'cloud-sellers' },
+      { name: 'Cloud products', route: 'cloud-search', param: 'all' },
+      { name: 'Help', route: 'help-page' },
+      { name: 'Dashboard', route: 'seller-dashboard' }
+    ]);
+  }
 });
+ 
