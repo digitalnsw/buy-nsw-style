446475c30d349cee40fbea0095bf4bc76b3d7559
Arman <arman.zrb@gmail.com>
2019-11-12 10:17:44 +1100

    Fix another typo

--- addon/components/dds/main-nav.js
+++ addon/components/dds/main-nav.js
@@ -14,7 +14,7 @@ export default Component.extend({
     return [
       {
         label: "Supplier list",
-        route: "cloud-sellers`",
+        route: "cloud-sellers",
       },
       {
         label: "Cloud products",
