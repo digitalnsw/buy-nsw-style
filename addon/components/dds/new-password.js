83be4c886e75858cefaad642b56f680dcfd87202
Arman <arman.zrb@gmail.com>
2019-11-12 15:31:25 +1100

    Improve new password component to handle confirmation

--- addon/components/dds/new-password.js
+++ addon/components/dds/new-password.js
@@ -2,11 +2,13 @@ import Component from '@ember/component';
 import layout from '../../templates/components/dds/new-password';
 import { computed } from '@ember/object';
 import { inject } from '@ember/service';
+import { next } from '@ember/runloop';
 
 export default Component.extend({
   layout,
   passwordStrength: inject(),
   hidePassword: true,
+  confirmation: '',
   length: computed('field', function() {
     let field = this.get('field');
     return field == undefined ? 0 : field.length;
@@ -15,12 +17,26 @@ export default Component.extend({
     const passwordStrength = this.get('passwordStrength');
     return passwordStrength.strengthSync(this.get('field')).score;
   }),
+  setValid() {
+    let self = this;
+    next(() => {
+      if (!this.isDestroyed) {
+        this.set('valid', 
+          (this.get('allowBlank') && this.get('length')==0 || this.get('strength') >= 3) &&
+          (!this.get('confirm') || this.get('field') == this.get('confirmation'))
+        );
+      }
+    });
+  },
+  didReceiveAttrs() {
+    this.setValid();
+  },
   actions: {
     togglePassword() {
       this.toggleProperty('hidePassword');
     },
     onChange() {
-      this.set('strong', this.get('allowBlank') && this.get('length')==0 || this.get('strength') >= 3);
+      this.setValid();
     }
   }
 });
